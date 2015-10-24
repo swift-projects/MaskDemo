d049c26f1a1268ff0be1819c643f59f81f7b8771
Timothy Lee <tim@codepath.com>
2015-10-24 15:18:24 -0700

    Initial Commit

---
+++ maskDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  maskDemo
+//
+//  Created by Timothy Lee on 10/24/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
